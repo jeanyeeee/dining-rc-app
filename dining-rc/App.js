73b2acfc0a63378ea58b6bc6cf819ddf50098058
Jean Tay <tayjeanyee@gmail.com>
2022-06-18 13:57:23 +0800

    home page top tabs complete

--- dining-rc/App.js
+++ dining-rc/App.js
@@ -6,10 +6,21 @@ import {View, StyleSheet} from 'react-native';
 
 const App = () => {
   return(
-      <NavigationContainer>
+    <NavigationContainer>
           <BottomTab />
+          <View style= {styles.container}>
+          </View>
       </NavigationContainer>
+
+
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FCFCFC",
+  }
+}
+)
--- dining-rc/navigation/screens/HomeScreen.js
+++ dining-rc/navigation/screens/HomeScreen.js
@@ -1,21 +1,78 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity, ScrollView} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-const HomeScreen  = ({navigation}) => {
+const Stack = createNativeStackNavigator();
+
+//MAIN: HomeScreen
+export default function HomeScreen() {
+  //Create the Button
+  const [dish, setDish] = useState(true)
   return(
     <View style= {styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+      {/* Header Buttons -  Popular Dishes, All Stalls */}
+      <View style= {styles.buttonArrangement}>
+        {/* Header Buttons -  Popular Dishes */}
+      <TouchableOpacity 
+        onPress={() => {
+          setDish(true)
+        }}>
+        <View style = {{...styles.leftButton,
+          backgroundColor: dish ? "#DFE2E5" : "#FCFCFC",
+          }}>
+            <Text style={styles.textAlt}>Popular Dishes</Text>
+          </View>
+    </TouchableOpacity>
+    {/* Header Buttons -  All Stalls */}
+    <TouchableOpacity 
+    onPress = {() => {
+      setDish(false)
+    }}>
+        <View style = {{...styles.rightButton,
+          backgroundColor: !dish ? "#DFE2E5" : "#FCFCFC",
+          }}>
+            <Text style={styles.textAlt}>All Stalls</Text>
+          </View>
+    </TouchableOpacity>
+      </View>
+        {/* Content */}
+      <ScrollView>
+
+      </ScrollView>
+      </View>
+
   ); 
 }
 
-export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#FCFCFC",
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  textAlt: {
+    color: "#0B735F",
+    textAlign: "center",
+    fontWeight:"bold",
+    fontSize: 16,
+  },
+  leftButton: {
+    padding: 20, 
+    borderRadius: 20, 
+  },
+  rightButton: {
+    padding: 20, 
+    borderRadius: 20, 
+  },
+
+  buttonArrangement:  {
+    backgroundColor: "#FCFCFC",
+    marginTop: 30, 
+    flexDirection: 'row', 
+    justifyContent: 'space-evenly'
+  }
 });
